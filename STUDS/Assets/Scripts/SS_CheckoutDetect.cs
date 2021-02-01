bf6f0628efa7b8a54f412063ba676199aa68e8c8
KIERAN BORLAND <u1136227@umail.utah.edu>
2021-02-01 02:56:56 -0700

    Implemented the item tracker for a player. Player now gets credit when checking out an item

---
+++ STUDS/Assets/Scripts/SS_CheckoutDetect.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SS_CheckoutDetect : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if(collision.collider.tag == "ShoppingItem")
+        {
+            GameObject player = collision.collider.gameObject.GetComponent<ShoppingItem>().GetPlayer();
+            string itemName = collision.collider.gameObject.GetComponent<ShoppingItem>().name;
+            string[] itemlist = player.GetComponent<SS_ItemTracker>().GetList();
+            for(int i = 0; i < itemlist.Length; i++)
+            {
+                if (itemlist[i].Equals(itemName))
+                {
+                    player.GetComponent<SS_ItemTracker>().CheckoutItem(i);
+                }
+            }
+            Destroy(collision.collider.gameObject);
+        }
+    }
+}
---
+++ STUDS/Assets/Scripts/ShoppingItem.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShoppingItem : MonoBehaviour
+{
+    private GameObject player;
+
+    public string name;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void SetPlayer(GameObject player) 
+    {
+        this.player = player;
+    }
+
+    public GameObject GetPlayer()
+    {
+        return player;
+    }
+}
