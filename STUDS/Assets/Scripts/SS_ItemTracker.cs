7e2634c24bf117be03e17ddc1b6f56ae54c827ea
Scott Mattes <scottmattes90@gmail.com>
2021-02-05 15:25:58 -0700

    Added Hammer & Hand Truck to Shopping Spree

--- STUDS/Assets/Scripts/SS_ItemTracker.cs
+++ STUDS/Assets/Scripts/SS_ItemTracker.cs
@@ -12,7 +12,7 @@ public class SS_ItemTracker : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        itemList = new string[] {"Propane", "Sprinkler", "Toolbox", "Boombox", "Cooler", "SlowSign", "Helmet", "Shovel"};
+        itemList = new string[] {"Propane", "Sprinkler", "Toolbox", "Boombox", "Cooler", "SlowSign", "Helmet", "Shovel", "Hammer",};
         completedItemsCheck = new bool[itemList.Length];
         
     }
