690a71058aced36d78609016f1afadcaffc7d0e3
Biraj <varsanivicky@gmail.com>
2021-03-02 02:35:06 -0700

    Working Volume Slider

---
+++ STUDS/Assets/Scripts/Sound/ChangeVolume.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ChangeVolume : MonoBehaviour
+{
+    public Slider thisSlider;
+    public float masterVolume;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    
+    public void SetSpecificVolume(string whatValue)
+    {
+        float sliderValue = thisSlider.value;
+        Debug.Log(sliderValue);
+
+        if (whatValue == "Master")
+        {
+            //Debug.Log("changed master volume to :" + thisSlider.value);
+            masterVolume = thisSlider.value;
+            AkSoundEngine.SetRTPCValue("MasterVolume", masterVolume);
+
+        }
+    }
+}
