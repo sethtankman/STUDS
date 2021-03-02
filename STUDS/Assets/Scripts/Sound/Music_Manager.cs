0a489b461ad11438c87a2a6cad175cf523aacfdb
Biraj <varsanivicky@gmail.com>
2021-03-01 17:13:45 -0700

    Music manager

---
+++ STUDS/Assets/Scripts/Sound/Music_Manager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Music_Manager : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public AK.Wwise.Event menuMusic;
+    public AK.Wwise.Event strollerMusic;
+    public AK.Wwise.Event shoppingMusic;
+
+
+    void Start()
+    {
+        DontDestroyOnLoad(this.gameObject);
+        PlayMusic("Start_Loop");
+    }
+    public void PlayMusic(string soundName)
+    {
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
