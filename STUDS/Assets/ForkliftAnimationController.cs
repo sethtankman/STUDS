0dadab6c46052189b205f389d15ce196d7bfb408
Scott Mattes <scottmattes90@gmail.com>
2021-03-04 17:24:33 -0700

    Removed pallet ramps, added forklifts w/ animations

---
+++ STUDS/Assets/ForkliftAnimationController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ForkliftAnimationController : MonoBehaviour
+{
+    public float timer;
+    public Animator _animator;
+
+    private void Update()
+    {
+        if (timer < 0)
+        {
+            // timer has run out = true
+            _animator.SetBool("timerHasRunOut", true);
+
+        }
+        else
+        {
+            timer -= Time.deltaTime;
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Player" && timer < 0)
+        {
+            _animator.SetTrigger("hasEntered");
+            timer = 5f;
+        }
+    }
+}
