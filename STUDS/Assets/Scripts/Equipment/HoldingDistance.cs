cbb04d32ce74bf49b4ee18815f4c250d8b5eb21f
Addison <addyshuppy@gmail.com>
2021-03-07 07:06:08 -0700

    Created Barebones HoldingDistance Script

---
+++ STUDS/Assets/Scripts/Equipment/HoldingDistance.cs
@@ -0,0 +1,8 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HoldingDistance : MonoBehaviour
+{
+    public float distance, height, leftRight;
+}
