600ee028c0355c799b9483da2e4302029942ce75
Chris Huckabee <slimeballgamesofficial@gmail.com>
2021-04-18 19:44:59 -0600

    Worked more on outside PB

---
+++ STUDS/Assets/Scripts/Menu/MouseRandomPick.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseRandomPick : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.None;
+        Cursor.visible = true;
+    }
+
+    public void HideMouse()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+
+    }
+}
