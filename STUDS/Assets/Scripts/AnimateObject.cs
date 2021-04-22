b4bb794e2956f9f61c611c9a8021b12d5119b787
KIERAN BORLAND <u1136227@umail.utah.edu>
2021-04-22 13:11:22 -0600

    Updated animate object script to now handle both material and game object toggle

--- STUDS/Assets/Scripts/AnimateObject.cs
+++ STUDS/Assets/Scripts/AnimateObject.cs
@@ -5,10 +5,17 @@ using UnityEngine;
 public class AnimateObject : MonoBehaviour
 {
     public Material[] animationMaterials;
+    public GameObject[] objectsToSwap;
     public float swapTime;
     public GameObject objectToAnimate;
 
+    public bool toggleMaterial;
+    public bool toggleGameObject;
+
+
+
     private int materialCounter;
+    private int objectCounter;
     private float timeCount;
     // Start is called before the first frame update
     void Start()
@@ -19,25 +26,58 @@ public class AnimateObject : MonoBehaviour
             materialCounter++;
         }
 
+        if(objectsToSwap.Length > 0)
+        {
+            objectsToSwap[0].SetActive(true);
+            for(int i = 1; i < objectsToSwap.Length; i++)
+            {
+                objectsToSwap[i].SetActive(false);
+            }
+            objectCounter++;
+        }
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        timeCount += Time.deltaTime;
-        if(timeCount >= swapTime)
+        if (toggleMaterial)
         {
-            if(animationMaterials.Length <= materialCounter)
+            timeCount += Time.deltaTime;
+            if (timeCount >= swapTime)
             {
-                objectToAnimate.GetComponent<MeshRenderer>().material = animationMaterials[0];
-                materialCounter = 1;
+                if (animationMaterials.Length <= materialCounter)
+                {
+                    objectToAnimate.GetComponent<MeshRenderer>().material = animationMaterials[0];
+                    materialCounter = 1;
+                }
+                else
+                {
+                    objectToAnimate.GetComponent<MeshRenderer>().material = animationMaterials[materialCounter];
+                    materialCounter++;
+                }
+                timeCount = 0;
             }
-            else
+        }else if (toggleGameObject)
+        {
+            timeCount += Time.deltaTime;
+            if (timeCount >= swapTime)
             {
-                objectToAnimate.GetComponent<MeshRenderer>().material = animationMaterials[materialCounter];
-                materialCounter++;
+                if (objectsToSwap.Length <= objectCounter)
+                {
+                    objectsToSwap[0].SetActive(true);
+                    objectsToSwap[objectsToSwap.Length - 1].SetActive(false);
+                    objectCounter = 1;
+                }
+                else
+                {
+                    objectsToSwap[objectCounter].SetActive(true);
+                    objectsToSwap[objectCounter - 1].SetActive(false);
+                    objectCounter++;
+                }
+                timeCount = 0;
             }
-            timeCount = 0;
         }
+
     }
 }
