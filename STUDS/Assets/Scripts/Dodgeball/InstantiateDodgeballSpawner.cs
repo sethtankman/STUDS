e8207a8dd764c2eb140ecf0a328a24bdf3471da4
Scott Mattes <scottmattes90@gmail.com>
2023-12-22 14:31:05 -0800

    Added dodgeball spawn FX, added light/medium/heavy ball types

--- STUDS/Assets/Scripts/Dodgeball/InstantiateDodgeballSpawner.cs
+++ STUDS/Assets/Scripts/Dodgeball/InstantiateDodgeballSpawner.cs
@@ -6,9 +6,13 @@ public class InstantiateDodgeballSpawner : MonoBehaviour
 {
     public GameObject DodgeballSpawner;
     public GameObject SpawnTransform;
+    public GameObject SpawnEffects;
 
     public void InstantiateDodgeballSpawn()
     {
+        Instantiate(SpawnEffects, SpawnTransform.transform.position, Quaternion.identity);
         Instantiate(DodgeballSpawner, SpawnTransform.transform.position, Quaternion.identity);
+       
+        Destroy(SpawnEffects, 5f);
     }
 }
