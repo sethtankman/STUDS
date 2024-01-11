11f45ab9cba2c018235e3b3c3f45c5ddb1b2384a
Scott Mattes <68486908+dadbodnokids@users.noreply.github.com>
2024-01-10 23:35:42 -0800

    Imported particles, updated dodgeball materials, cleaned up main folder
    Randomized time of day script now has the option to rain at night (50/50 chance)

--- STUDS/Assets/RandomizeTimeOfDay.cs
+++ STUDS/Assets/RandomizeTimeOfDay.cs
@@ -7,23 +7,30 @@ public class RandomizeTimeOfDay : MonoBehaviour
     public GameObject Day;
     public GameObject Sunset;
     public GameObject Night;
-    public float randomNumber;
+    public GameObject Rain;
+    public float RandomSky;
+    public float RandomRain;
 
     void Start()
     {
-        randomNumber = Random.Range(1, 4);
+        RandomSky = Random.Range(1, 4);
+        RandomRain = Random.Range(1, 3);
 
-        if (randomNumber == 1)
+        if (RandomSky == 1)
         {
             Day.SetActive(true);
         }
-        else if (randomNumber == 2)
+        else if (RandomSky == 2)
         {
             Sunset.SetActive(true);
         }
         else
         {
             Night.SetActive(true);
+            if (RandomRain == 2)
+            {
+                Rain.SetActive(true);
+            }
         }
     }
 }
