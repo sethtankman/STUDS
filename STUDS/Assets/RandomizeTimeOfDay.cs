8833a425c3dd50e01fdc44ef3d850a896af494a5
Scott Mattes <scottmattes90@gmail.com>
2024-01-16 14:50:12 -0800

    Added fog to random time of day. Saved rain prefab

--- STUDS/Assets/RandomizeTimeOfDay.cs
+++ STUDS/Assets/RandomizeTimeOfDay.cs
@@ -4,17 +4,23 @@ using UnityEngine;
 
 public class RandomizeTimeOfDay : MonoBehaviour
 {
+    //gameobjects
     public GameObject Day;
     public GameObject Sunset;
     public GameObject Night;
     public GameObject Rain;
+    public GameObject Fog;
+
+    //random numbers
     public float RandomSky;
     public float RandomRain;
+    public float RandomFog;
 
     void Start()
     {
         RandomSky = Random.Range(1, 4);
         RandomRain = Random.Range(1, 3);
+        RandomFog = Random.Range(1, 3);
 
         if (RandomSky == 1)
         {
@@ -23,6 +29,10 @@ public class RandomizeTimeOfDay : MonoBehaviour
         else if (RandomSky == 2)
         {
             Sunset.SetActive(true);
+            if (RandomFog == 2)
+            {
+                Fog.SetActive(true);
+            }
         }
         else
         {
@@ -30,6 +40,10 @@ public class RandomizeTimeOfDay : MonoBehaviour
             if (RandomRain == 2)
             {
                 Rain.SetActive(true);
+            }
+            else if (RandomFog == 2)
+            {
+                Fog.SetActive(true);
             }
         }
     }
