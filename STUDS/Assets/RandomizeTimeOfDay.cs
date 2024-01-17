7308aeeb5c239d449abe214fcb8959cf41d1ff09
Scott Mattes <scottmattes90@gmail.com>
2024-01-16 18:07:34 -0800

    RandomizeTimeOfDay script organization + debug options
    You can now debug enable manual time of day and weather options for testing. Sunset and Night time can now be both foggy and rainy

--- STUDS/Assets/RandomizeTimeOfDay.cs
+++ STUDS/Assets/RandomizeTimeOfDay.cs
@@ -4,47 +4,115 @@ using UnityEngine;
 
 public class RandomizeTimeOfDay : MonoBehaviour
 {
-    //gameobjects
+    //debug booleans
+    [Header("Enable/Disable Debug")]
+    [Tooltip("Enable manual selection of time, weather")]
+    public bool debugEnabled = false;
+
+    [Header("Debug Options")]
+    public bool setDay = false;
+    public bool setSunset = false;
+    public bool setNight = false;
+    public bool setRaining = false;
+    public bool setFoggy = false;
+
+    [Header("Randomized options")]
+    [Tooltip("Enable randomized time, weather -- disables debug")]
+    public bool randomEnabled = true;
+
+    //random numbers
+    [Header("Debug View Random Number Values")]
+    [Tooltip("View the value of floats that determine time, weather at runtime")]
+    public float RandomSky;
+    public float RandomRain;
+    public float RandomFog;
+
+    //day, night, FX game objects
+    [Header("Time and Weather GameObjects")]
+    [Tooltip("Manually select time of day, weather")]
     public GameObject Day;
     public GameObject Sunset;
     public GameObject Night;
     public GameObject Rain;
     public GameObject Fog;
 
-    //random numbers
-    public float RandomSky;
-    public float RandomRain;
-    public float RandomFog;
-
     void Start()
     {
-        RandomSky = Random.Range(1, 4);
-        RandomRain = Random.Range(1, 3);
-        RandomFog = Random.Range(1, 3);
+        //set day, night, FX based on script booleans
+        if (debugEnabled)
+        {
+            randomEnabled = false;
 
-        if (RandomSky == 1)
-        {
-            Day.SetActive(true);
-        }
-        else if (RandomSky == 2)
-        {
-            Sunset.SetActive(true);
-            if (RandomFog == 2)
+            if (setDay)
             {
-                Fog.SetActive(true);
+                Day.SetActive(true);
             }
-        }
-        else
-        {
-            Night.SetActive(true);
-            if (RandomRain == 2)
+            else if (setSunset)
+            {
+                Sunset.SetActive(true);
+            }
+            else if (setNight)
+            {
+                Night.SetActive(true);
+            }
+            if (setRaining)
             {
                 Rain.SetActive(true);
             }
-            else if (RandomFog == 2)
+            if (setFoggy)
             {
                 Fog.SetActive(true);
             }
+
+        }
+        //set day, night, FX at random
+        else if (randomEnabled)
+        {
+            DisableDebug();
+
+            RandomSky = Random.Range(1, 4);
+            RandomRain = Random.Range(1, 3);
+            RandomFog = Random.Range(1, 3);
+
+            //day
+            if (RandomSky == 1)
+            {
+                Day.SetActive(true);
+            }
+            //sunset + weather
+            else if (RandomSky == 2)
+            {
+                Sunset.SetActive(true);
+                SetWeather();
+            }
+            //night + weather
+            else
+            {
+                Night.SetActive(true);
+                SetWeather();
+            }
+        }
+    }
+
+    void DisableDebug()
+    {
+        debugEnabled = false;
+        setDay = false;
+        setSunset = false;
+        setNight = false;
+        setFoggy = false;
+        setRaining = false;
+    }
+
+    void SetWeather()
+    {
+        if (RandomFog == 2)
+        {
+            Fog.SetActive(true);
+        }
+        if (RandomRain == 2)
+        {
+            Rain.SetActive(true);
         }
     }
 }
