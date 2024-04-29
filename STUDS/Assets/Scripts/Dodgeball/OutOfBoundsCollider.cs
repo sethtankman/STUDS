c424a8146e680d2651f56372535d2815d24dd08f
Addison <addyshuppy@gmail.com>
2024-04-28 22:32:37 -0600

    Out of bounds punishment.

---
+++ STUDS/Assets/Scripts/Dodgeball/OutOfBoundsCollider.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OutOfBoundsCollider : MonoBehaviour
+{
+    [SerializeField] private Transform[] Spawns;
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("T");
+        if (other.gameObject.CompareTag("Player"))
+        {
+            other.gameObject.GetComponent<CharacterController>().enabled = false;
+            other.transform.position = Spawns[Random.Range(0, 3)].position;
+            other.gameObject.GetComponent<CharacterController>().enabled = true;
+            DBGameManager.Instance.AddPoints(other.gameObject.GetComponent<CharacterMovementController>().color, -20);
+        }
+    }
+}
