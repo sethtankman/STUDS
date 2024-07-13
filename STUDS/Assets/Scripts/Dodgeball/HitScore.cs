99e7285d02d87158b99f1d98c3bce737e5a991ab
Addison <addyshuppy@gmail.com>
2024-07-13 13:00:44 -0600

    Dodgeballs are now destroyed on hit

--- STUDS/Assets/Scripts/Dodgeball/HitScore.cs
+++ STUDS/Assets/Scripts/Dodgeball/HitScore.cs
@@ -6,9 +6,14 @@ public class HitScore : MonoBehaviour
 {
     public int pointValue;
 
+    /// <summary>
+    /// Records the hit and destroys the parent gameobject
+    /// </summary>
+    /// <param name="owner"></param>
     public void RecordHit(string owner)
     {
         if(owner.Length > 0)
             DBGameManager.Instance.AddPoints(owner, pointValue);
+        Destroy(transform.parent.gameObject);
     }
 }
