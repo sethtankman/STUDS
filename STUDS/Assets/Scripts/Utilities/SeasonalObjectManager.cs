6550a36b84c946aff2bcd143f21537077ad9d8ab
Scott Mattes <scottmattes90@gmail.com>
2025-02-27 13:44:45 -0800

    SeasonalObjectManager debug options | imported temp pumpkin props

--- STUDS/Assets/Scripts/Utilities/SeasonalObjectManager.cs
+++ STUDS/Assets/Scripts/Utilities/SeasonalObjectManager.cs
@@ -3,6 +3,15 @@ using System;
 
 public class SeasonalObjectManager : MonoBehaviour
 {
+    [Header("Debug")]
+    [SerializeField]
+    private bool enableSeasons = true;
+    [SerializeField]
+    private bool debugSeasons = false;
+    [SerializeField]
+    int month;
+
+
     [Header("Seasonal Objects")]
     public GameObject springObjects;
     public GameObject summerObjects;
@@ -11,13 +20,58 @@ public class SeasonalObjectManager : MonoBehaviour
 
     void Start()
     {
-        EnableSeasonalObjects();
+        // Only enable seasons if bool is enabled
+        if(enableSeasons)
+        {
+            EnableSeasonalObjects();
+        }
+        else
+        {
+            springObjects?.SetActive(false);
+            summerObjects?.SetActive(false);
+            fallObjects?.SetActive(false);
+            winterObjects?.SetActive(false);
+        }
+        
+        
+        // Debug enable a specified month (be sure to set enableSeasons bool to false first)
+        if(debugSeasons)
+        {
+            DebugSeasonalObjects();
+        }
     }
 
     void EnableSeasonalObjects()
     {
-        int month = DateTime.Now.Month; // Get current month
+        month = DateTime.Now.Month; // Get current month
 
+        // Disable all objects first
+        springObjects?.SetActive(false);
+        summerObjects?.SetActive(false);
+        fallObjects?.SetActive(false);
+        winterObjects?.SetActive(false);
+
+        // Enable the correct season's objects
+        if (month >= 3 && month <= 5) // March - May (Spring)
+        {
+            springObjects?.SetActive(true);
+        }
+        else if (month >= 6 && month <= 8) // June - August (Summer)
+        {
+            summerObjects?.SetActive(true);
+        }
+        else if (month >= 9 && month <= 11) // September - November (Fall)
+        {
+            fallObjects?.SetActive(true);
+        }
+        else // December - February (Winter)
+        {
+            winterObjects?.SetActive(true);
+        }
+    }
+
+    void DebugSeasonalObjects()
+    {
         // Disable all objects first
         springObjects?.SetActive(false);
         summerObjects?.SetActive(false);
