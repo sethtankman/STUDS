5b74fe7ce0ff94c2ec1942cc4671fdb08ae918cb
Addison <addyshuppy@gmail.com>
2025-03-02 10:55:37 -0700

    Moved Some Scripts into folders

---
+++ STUDS/Assets/Scripts/LevelSelect/HubColorChange.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HubColorChange : MonoBehaviour
+{
+    public Material playerColor;
+
+    [SerializeField] private GameObject gameManager;
+
+    public string colorName; // Set in editor
+
+    public AK.Wwise.Event ParticleSound;
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(!gameManager)
+            gameManager = GameObject.Find("NetGameManager");
+            if (!gameManager)
+                gameManager = GameObject.Find("GameManager");
+        if (other.CompareTag("Player"))
+        {
+            if (gameManager.GetComponent<ManagePlayerHub>())
+            {
+                List<GameObject> players = gameManager.GetComponent<ManagePlayerHub>().getPlayers();
+
+                bool isTaken = false;
+                foreach (GameObject player in players)
+                {
+                    if (player && player.GetComponent<CharacterMovementController>().GetColorName().Equals(colorName))
+                    {
+                        isTaken = true;
+                    }
+                }
+                if (!isTaken)
+                {
+                    ParticleSound.Post(gameObject);
+                    other.gameObject.GetComponentInChildren<SkinnedMeshRenderer>().material = playerColor;
+                    other.GetComponent<CharacterMovementController>().SetColorName(colorName);
+                }
+            }
+            else if (gameManager.GetComponent<NetGameManager>())
+            {
+                GameObject[] players = GameObject.FindGameObjectsWithTag("Player");
+
+                bool isTaken = false;
+                foreach (GameObject player in players)
+                {
+                    if (player.GetComponent<NetworkCharacterMovementController>().GetColorName().ToLower().Equals(colorName.ToLower()))
+                    {
+                        isTaken = true;
+                    }
+                }
+                if (!isTaken)
+                {
+                    ParticleSound.Post(gameObject);
+                    other.gameObject.GetComponentInChildren<SkinnedMeshRenderer>().material = playerColor;
+                    other.GetComponent<NetworkCharacterMovementController>().SetColorName(colorName);
+                }
+            }
+        }
+    }
+}
---
+++ STUDS/Assets/Scripts/Obstacles/KnockBack.cs
@@ -0,0 +1,125 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KnockBack : MonoBehaviour
+{
+    public Vector3 directionVector;
+    public short KBForce;
+    public bool makePlayerDrop;
+    public AK.Wwise.Event KBSound;
+    public bool slidethrough = false, reflective, directional;
+    private Collider PaintColl;
+    public string owner;
+
+    public GameObject KnockBackFX;
+    
+
+    private void Start()
+    {
+        PaintColl = gameObject.GetComponent<BoxCollider>();
+        if(PaintColl == null)
+        {
+            PaintColl = gameObject.GetComponent<MeshCollider>();
+        }
+    }
+
+    public void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            if (other.GetComponent<CharacterMovementController>())
+            {
+                if (GetComponentInParent<StrollerController>())
+                {
+                    string otherColor = other.GetComponent<CharacterMovementController>().GetColorName();
+                    string myColor = gameObject.GetComponentInParent<StrollerController>().GetColor();
+                    if (otherColor != null && myColor.Equals(otherColor))
+                    {
+                        return;
+                    }
+                }
+                else if (GetComponentInParent<GrabbableObjectController>())
+                {
+                    GetComponentInParent<GrabbableObjectController>().HomingThrow(false, null, Vector3.zero);
+                    Debug.Log($"1: {GetComponentInParent<GrabbableObjectController>().throwerColor}, 2: {other.GetComponent<CharacterMovementController>().GetColorName()}");
+                    if (GetComponentInParent<GrabbableObjectController>().throwerColor == other.GetComponent<CharacterMovementController>().GetColorName())
+                    {
+                        return;
+                    }
+                }
+                Vector3 direction = Vector3.zero; //Temporarily set.
+                if (reflective)
+                {
+                    direction += (other.transform.position - transform.position).normalized;
+                }
+                if (directional)
+                {
+                    directionVector = directionVector.normalized;
+                    direction.x *= 1 - directionVector.x;
+                    direction.y *= 1 - directionVector.y;
+                    direction.z *= 1 - directionVector.z;
+                    direction += directionVector.normalized;
+                }
+                Debug.Log("direction: " + direction.ToString() + " Force " + KBForce + " MakePlayerDrop " + makePlayerDrop);
+
+                other.GetComponent<CharacterMovementController>().KnockBack(direction * KBForce, makePlayerDrop);
+                if(KnockBackFX)
+                    Instantiate(KnockBackFX, other.transform.position, Quaternion.identity);
+                if (GetComponent<HitScore>())
+                    GetComponent<HitScore>().RecordHit(GetComponentInParent<GrabbableObjectController>().throwerColor);
+                if (!KBSound.Equals(""))
+                {
+                    KBSound.Post(gameObject);
+                }
+                if (slidethrough)
+                {
+                    StartCoroutine(ColliderToggle());
+                }
+            }
+            else if (other.GetComponent<NetworkCharacterMovementController>())
+            {
+                // Cancel hit if your own item hits you (network only)
+                if (owner.Equals(other.name))
+                {
+                    return;
+                }
+
+
+                Vector3 direction = Vector3.zero; //Temporarily set.
+                if (reflective)
+                {
+                    direction += (other.transform.position - transform.position).normalized;
+                }
+                if (directional)
+                {
+                    direction += directionVector.normalized;
+                }
+
+                other.gameObject.GetComponent<NetworkCharacterMovementController>().KnockBack(direction * KBForce, makePlayerDrop);
+
+                if (KnockBackFX)
+                    Instantiate(KnockBackFX, other.transform.position, Quaternion.identity); 
+                if (GetComponent<HitScore>())
+                    GetComponent<HitScore>().RecordHit(GetComponentInParent<NetGrabbableObjectController>().throwerColor);
+                if (!KBSound.Equals(""))
+                {
+                    KBSound.Post(gameObject);
+                }
+                if (slidethrough)
+                {
+                    StartCoroutine(ColliderToggle());
+                }
+            }
+        }
+    }
+
+    IEnumerator ColliderToggle()
+    {
+        PaintColl.enabled = false;
+        yield return new WaitForSeconds(2);
+        PaintColl.enabled = true;
+        yield return null;
+    }
+}
+ 
