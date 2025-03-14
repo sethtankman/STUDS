9ca52f636703fec0bd8dcc89b7144ac27c3eb024
Scott Mattes <scottmattes90@gmail.com>
2025-03-14 08:49:39 -0700

    Added Easter to the holiday script

--- STUDS/Assets/Scripts/Utilities/HolidayObjectManager.cs
+++ STUDS/Assets/Scripts/Utilities/HolidayObjectManager.cs
@@ -14,6 +14,7 @@ public class HolidayObjectManager : MonoBehaviour
     public GameObject newYearsObjects;
     public GameObject valentinesObjects;
     public GameObject stPatricksObjects;
+    public GameObject easterObjects;
     public GameObject independenceObjects;
     public GameObject halloweenObjects;
     public GameObject thanksgivingObjects;
@@ -52,7 +53,8 @@ public class HolidayObjectManager : MonoBehaviour
             { "Independence Day", (new DateTime(currentYear, 7, 4), independenceObjects) },
             { "Halloween", (new DateTime(currentYear, 10, 31), halloweenObjects) },
             { "Thanksgiving", (GetThanksgivingDate(currentYear), thanksgivingObjects) },
-            { "Christmas", (new DateTime(currentYear, 12, 25), christmasObjects) }
+            { "Christmas", (new DateTime(currentYear, 12, 25), christmasObjects) },
+            { "Easter", (GetEasterDate(currentYear), easterObjects) }
         };
     }
 
@@ -99,5 +101,25 @@ public class HolidayObjectManager : MonoBehaviour
         DateTime firstDayOfNovember = new DateTime(year, 11, 1);
         int daysUntilThursday = (DayOfWeek.Thursday - firstDayOfNovember.DayOfWeek + 7) % 7;
         return firstDayOfNovember.AddDays(daysUntilThursday + (3 * 7));
+    }
+
+    private static DateTime GetEasterDate(int year)
+    {
+        // Computus algorithm to calculate Easter Sunday
+        int a = year % 19;
+        int b = year / 100;
+        int c = year % 100;
+        int d = b / 4;
+        int e = b % 4;
+        int f = (b + 8) / 25;
+        int g = (b - f + 1) / 3;
+        int h = (19 * a + b - d - g + 15) % 30;
+        int i = c / 4;
+        int k = c % 4;
+        int l = (32 + 2 * e + 2 * i - h - k) % 7;
+        int m = (a + 11 * h + 22 * l) / 451;
+        int month = (h + l - 7 * m + 114) / 31;
+        int day = ((h + l - 7 * m + 114) % 31) + 1;
+        return new DateTime(year, month, day);
     }
 }
