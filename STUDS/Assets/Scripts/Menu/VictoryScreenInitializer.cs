3654b6d9c1b00b6fc80f1e74cbec0ed3cf51a9ed
Addison <addyshuppy@gmail.com>
2025-04-28 21:12:39 -0600

    Changed Penny Pincher requirement to 150 bucks with 1 kid minimum

--- STUDS/Assets/Scripts/Menu/VictoryScreenInitializer.cs
+++ STUDS/Assets/Scripts/Menu/VictoryScreenInitializer.cs
@@ -20,6 +20,13 @@ public class VictoryScreenInitializer : MonoBehaviour
         GameObject.Find("Music Manager").GetComponent<Music_Manager>().PlayStopMusic("Penny", false);
         GameObject.Find("Music Manager").GetComponent<Music_Manager>().PlayStopMusic("Dodgeball", false);
         GameObject.Find("Music Manager").GetComponent<Music_Manager>().PlayStopMusic("Menu", true);
+
+        finalText = GameObject.Find("PBFinalText(Clone)");
+        if (finalText)
+        {
+            DetermineFinalText();
+        }
+
         if (ManagePlayerHub.Instance)
         {
             var players = ManagePlayerHub.Instance.getPlayers();
@@ -36,21 +43,19 @@ public class VictoryScreenInitializer : MonoBehaviour
                 player.SetActive(false);
             }
         }
-
-
-        finalText = GameObject.Find("PBFinalText(Clone)");
-        if (finalText)
-        {
-            DetermineFinalText();
-        }
     }
 
     private void DetermineFinalText()
     {
         finalText.transform.SetParent(GameObject.Find("Canvas").transform);
         double score = Double.Parse(finalText.GetComponent<TextMeshProUGUI>().text);
-        finalText.GetComponent<TextMeshProUGUI>().text = "Power Bill: $" + (score / 10) + "0";
+        finalText.GetComponent<TextMeshProUGUI>().text = $"Power Bill: ${score / 10}0";
         finalText.GetComponent<RectTransform>().anchoredPosition = new Vector3(10, -10, 0);
+        int childCount = 0;
+        foreach (CharacterMovementController cmc in FindObjectsByType<CharacterMovementController>(0))
+        {
+            if (cmc.isMini) { childCount++; }
+        }
         if(score > 4000)
         {
             finalText.GetComponent<TextMeshProUGUI>().text += " When your credit card is stolen but your credit starts to improve...";
@@ -65,8 +70,12 @@ public class VictoryScreenInitializer : MonoBehaviour
             finalText.GetComponent<TextMeshProUGUI>().text += " Time for that new boat!";
         } else
         {
-            SteamAchievements.UnlockAchievement("PB_THRIFTY");
             finalText.GetComponent<TextMeshProUGUI>().text += " My mother-in-law will have nothing to say...";
+        }
+        if (score < 1500 && childCount > 0)
+        {
+            Debug.Log("Achieved");
+            SteamAchievements.UnlockAchievement("PB_THRIFTY");
         }
     }
 }
